84d50119686728154d6936b9f79e7475974d20e4
mudit04 <muditmangal05@gmail.com>
2023-05-22 02:30:41 +0530

    fix bug gpg generator

--- gpg_key_automator.sh
+++ gpg_key_automator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function gitlink(){
-    git config --global user.signingkey $(1)
+    git config --global user.signingkey $1
     git config --global commit.gpgsign true
 }
 
@@ -11,43 +11,39 @@ echo 3 exit
 
 read -p "Input: " selection
 
-echo ${selection}
+# echo ${selection}
 
 if [[ selection -eq "1" ]]
 then
-key= $(gpg --list-secret-keys --keyid-format=long | awk '/sec/ {print $2}')
+key=($(gpg --list-secret-keys --keyid-format=long|awk '/sec/{print $2}'))
 
-for (( i=1 ; i<${#key} ; i++ )) do
-echo ${i} . $((key[${i}]):5:16)
+for (( i=0 ; i<${#key[@]} ; i++ )) do
+rsa=${key[$i]}
+echo $i  "."  ${rsa:8:16}
 
 
 done
 
-rsa_key=$((key[${index}]):5:16)
-echo ""
 read -p "Select the key you want to use: " index
 
-gpg --armor --export "$rsa_key"
+keyParse=${key[$index]}
+
+rsa_key=${keyParse:8:16}
+
+gpg --armor --export $rsa_key
 
 gitlink $rsa_key
 
 elif [[ selection -eq "2" ]] 
 then
-gpg --gen-key
-key= $(gpg --list-secret-keys --keyid-format=long | awk '/sec/ {print $2}')
+gpg --full-generate-key
+key=$(gpg --list-secret-keys --keyid-format=long | awk '/sec/ {print $2}')
 rsa_key=$(echo ${key[@]} | tail -c 17)
-gpg --armor --export "$rsa_key"
+gpg --armor --export $rsa_key
 
 gitlink $rsa_key
 
-elif [[ selection -eq "3" ]]
-then
-
-break
-
 else
-echo Ha chal apne baap ko mat sikha
+echo Bye!!
 
 fi
-
-
